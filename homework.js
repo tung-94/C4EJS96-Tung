af4ee3292eaa154f789be7e00fddef918368e30d
thanhtung-1994 <img>
2020-09-15 23:07:45 +0700

    creat new project

---
+++ homework.js
@@ -0,0 +1,67 @@
++ Bài 1:
+console.log("Trong JS xảy ra lỗi sytax khi")
+console.log("Biến được khởi tạo bắt đầu bằng số")
+console.log("Biến chứa những ký tự đặc biệt")
+console.log("Biến trùng với các từ khóa đặc biệt trong JS")
+console.log("Ví dụ")
+console.log("Let 5ting")
+console.log("T@N")
+console.log("Let while")
+console.log("Trong JS muốn kiểm tra một kiểu dữ liệu trong biến ta dùng lệnh typeof")
++ Bài 2:
+console.log("1:b")
+console.log("2:a")
+console.log("3:e")
+console.log("4:c,d")
++ Bài 3:
+let message = "coding is great"
+console.log(message)
+let studentcount = 0
+console.log(studentcount)
++ Bài 4:
+let message = "coding is great"
+message ="coding might not be easy,but still great";
+console.log(message);
+
+let studentcount = 0;
+studentcount = 16;
+console.log(studentcount)
+
+let message ="coding might not be easy,but still great";
+console.log(message);
+message = message. toLowerCase();
+console.log(message);
+
+let studentcount = 0;
+studentcount = 16;
+console.log(studentcount);
+studentcount++;
+console.log(studentcount);
+
++ Bài 5:
+alert("Chúc ngày mới tốt lành")
+
++ Bài 6:
+let name = prompt("Hi there,your name please!");
+alert(Xin chào bạn $(name));
+
++ Bài 7:
+let firstName = prompt("Enter your first name");
+let name = prompt("Enter your last name");
+alert('Hi $(firstName) $(lastName)');
+
++ Bài 8:
+let x=prompt("the lenghth of the squares??");
+let y=x*x
+alert('the area of the squares is $ (y)');
+
++ Bài 9:
+let r=prompt("bán kính hình tròn");
+let pi=3,14
+let s=r*r*pi
+alert('diện tích hình tròn$(s)');
+
++ Bài 10:
+var x=prompt("Nhập vào do C");
+var y=x*1,8+32;
+alert('$(x)(C) = $(y) (F)');
